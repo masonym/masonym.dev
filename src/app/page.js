5419a2baba756ff0ec1d09da55e0bbf397520d69
Mason Leitch <nosamleitch@gmail.com>
2024-08-19 11:08:38 -0700

    Fixed scaling of main sections

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import NavigationList from "@/components/Navigation/NavigationList";
 export default function Home() {
   return (
     <div className={styles.App}>
-      <div>
+      <div style={{display: "flex", flexDirection: 'column', alignItems: 'center'}}>
         {navigationItems.map((section, index) => (
           <Section key={index} title={section.title}>
             <NavigationList
