84867445a9fe2483e1ee6f7cdcaf6e05d1b84bf9
Mason Leitch <nosamleitch@gmail.com>
2024-08-19 21:39:13 -0700

    Added state for inputs

--- src/app/hexa/components/SkillItem/SkillItem.jsx
+++ src/app/hexa/components/SkillItem/SkillItem.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import styles from './SkillItem.module.css'; // New CSS module for SkillItem
 
-export const SkillItem = ({ skills, altText, classKey, isCommon, itemStyle }) => {
-    // Determine the correct image path
+export const SkillItem = ({ skills, altText, classKey, isCommon, itemStyle, onInputChange }) => {
+    const [inputValue, setInputValue] = useState(null);
+
+    // image handling for common cores
     const imagePath = isCommon
         ? `/common/${altText}.png`
         : `/classImages/${classKey}/Skill_${altText}.png`;
+
+    // input field
+    const handleInputChange = (event) => {
+        let value = parseInt(event.target.value, 10);
+
+        if (isNaN(value)) {
+            value = 0;
+        } else if (value > 30) {
+            value = 30;
+        } else if (value < 0) {
+            value = 0;
+        }
+
+        setInputValue(value);
+        onInputChange && onInputChange(altText, value); 
+    };
 
     return (
         <div className={`${styles.item} ${itemStyle}`}>
@@ -19,6 +37,14 @@ export const SkillItem = ({ skills, altText, classKey, isCommon, itemStyle }) =>
             <span style={{ whiteSpace: 'pre-line' }}>
                 <p>{Array.isArray(skills) ? skills.join('\n') : skills}</p>
             </span>
+            <input
+                // type="number"
+                value={inputValue}
+                onChange={handleInputChange}
+                min={0}
+                max={30}
+                className={styles.levelInput}
+            />
         </div>
     );
 };
