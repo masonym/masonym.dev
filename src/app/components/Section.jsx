10b0edd7b3e77e6ceb0932b3ab5c8052f8066ba9
Mason Leitch <nosamleitch@gmail.com>
2024-08-25 06:46:19 -0700

    More tailwind updating

--- src/app/components/Section.jsx
+++ src/app/components/Section.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Section = ({ title, children }) => {
   return (
-    <div className="border border-primary-dim rounded-md w-[65%] mt-8 text-left px-5">
-      <h2>{title}</h2>
+    <div className="border border-primary-dim rounded-md w-[65%] mt-8 text-left p-5">
+      <h2 className="m-2 mb-6 text-xl">{title}</h2>
       {children}
     </div>
   );
--- src/components/Navigation/NavigationList.jsx
+++ src/components/Navigation/NavigationList.jsx
@@ -1,37 +1,28 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import styles from '@/app/page.module.css'
 
 const NavigationList = ({ items, onClose, showImages = false, alignItemsProp = 'flex-start', flexDirectionProp = "column", rowGapProp = 0 }) => {
     return (
-        <ul style={{
-            display: 'flex',
-            alignItems: alignItemsProp,
-            flexDirection: flexDirectionProp,
-            justifyContent: "flex-start",
-            flexWrap: 'wrap',
-            rowGap: rowGapProp
-        }}>
+        <ul className={`flex items-${alignItemsProp} flex-${flexDirectionProp} justify-start flex-wrap gap-4`} style={{ rowGap: rowGapProp }}>
             {items.map((item, index) => (
                 <Link key={item.href} href={item.href} onClick={onClose}>
-                    <li key={index} className={styles.pageButton}>
+                    <li className="flex items-center py-4 pl-2 h-16 pr-8 hover:bg-primary-dark hover:rounded-lg">
                         {showImages && item.image && (
                             <Image
                                 src={item.image}
                                 alt={item.name}
-                                width={0}
-                                height={0}
+                                width={32}
+                                height={32}
+                                className="mr-8"
                                 unoptimized
-                                style={{ marginRight: 32, width: '32px', height: 'auto' }} // optional
                             />
                         )}
                         {item.name}
                     </li>
                 </Link>
-            ))
-            }
-        </ul >
+            ))}
+        </ul>
     );
 };
 
