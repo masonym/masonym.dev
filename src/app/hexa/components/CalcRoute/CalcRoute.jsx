a53b8b0c5b534e2a0b7ebbb4d23b0147f62d7991
Mason Leitch <nosamleitch@gmail.com>
2024-09-09 22:49:18 -0700

    Added local storage to calc route to remember calc choice

--- src/app/hexa/components/CalcRoute/CalcRoute.jsx
+++ src/app/hexa/components/CalcRoute/CalcRoute.jsx
@@ -1,5 +1,5 @@
 import { Container } from 'postcss'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Optimizer from './Optimizer'
 import CostCalc from './CostCalc'
 import { InputGrid } from '../InputGrid/InputGrid'
@@ -7,30 +7,47 @@ import { InputGrid } from '../InputGrid/InputGrid'
 
 const CalcRoute = ({ selectedClass, classDetails, skillLevels }) => {
   const [selected, setSelected] = useState(null)
+
+  useEffect(() => {
+    // Load the selected calculator from local storage when the component mounts
+    const savedCalculator = localStorage.getItem('selectedCalculator')
+    if (savedCalculator) {
+      setSelected(savedCalculator)
+    }
+  }, [])
+
+  const handleSelectCalculator = (calculator) => {
+    setSelected(calculator)
+    // Save the selected calculator to local storage
+    localStorage.setItem('selectedCalculator', calculator)
+  }
+
   return (
     <div className="flex flex-col items-center gap-4">
 
       <h3 className="mt-4 text-[20px] text-center">Select the calculator you'd like to use:</h3>
       <div className="flex flex-col md:flex-row flexBetween max-container mt-2 gap-4 text-nowrap">
 
-        <button className="flex-1 text-[24px] text-primary bg-primary-dark border-primary-dim border-[1px] hover:bg-primary-dim transition-all duration-300 rounded-lg px-8 py-4" onClick={() => { setSelected('costCalc') }}>Cost Calculator</button>
-        <button className="flex-1 text-[24px] text-primary bg-primary-dark border-primary-dim border-[1px] hover:bg-primary-dim transition-all duration-300 rounded-lg px-8 py-4" onClick={() => { setSelected('optimizer') }}>Level Priority</button>
+        <button
+          className="flex-1 text-[24px] text-primary bg-primary-dark border-primary-dim border-[1px] hover:bg-primary-dim transition-all duration-300 rounded-lg px-8 py-4"
+          onClick={() => { handleSelectCalculator('costCalc') }}>
+          Cost Calculator
+        </button>
+        <button className="flex-1 text-[24px] text-primary bg-primary-dark border-primary-dim border-[1px] hover:bg-primary-dim transition-all duration-300 rounded-lg px-8 py-4"
+          onClick={() => { handleSelectCalculator('optimizer') }}>
+          Level Priority
+        </button>
 
       </div>
       <div className="flex flex-col mb-4">
-        {
-          (selected == 'costCalc') ?
-            <CostCalc
-              selectedClass={selectedClass}
-              classDetails={classDetails}
-              skillLevels={skillLevels}
-            >
-
-            </CostCalc> : null
-        }
-        {
-          (selected == 'optimizer') ? <Optimizer /> : null
-        }
+        {selected === 'costCalc' && (
+          <CostCalc
+            selectedClass={selectedClass}
+            classDetails={classDetails}
+            skillLevels={skillLevels}
+          />
+        )}
+        {selected === 'optimizer' && <Optimizer />}
       </div>
 
 
