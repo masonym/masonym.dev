50b3e580b23038a420e94113ad74baf510a56333
Mason Leitch <nosamleitch@gmail.com>
2025-01-07 09:19:04 -0800

    Added google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import NavBar from "@/components/Navigation/NavBar/NavBar";
 import Footer from "@/components/Footer";
 import DynamicFavicon from "./components/DynamicFavicon";
+import Script from "next/script";
 
 export const metadata = {
   title: "mason's maple matrix",
@@ -13,6 +14,18 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link id="favicon" rel="icon" href="/favicon.ico" />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-BY50PB72SB"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BY50PB72SB');
+          `}
+        </Script>
       </head>
       <body className="min-h-screen">
         <DynamicFavicon />
