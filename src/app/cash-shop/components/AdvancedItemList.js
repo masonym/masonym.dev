c89ffd8d7c726dab049d7ccbbdb390c8d21f2071
Mason Leitch <nosamleitch@gmail.com>
2025-09-24 13:12:41 -0700

    updated v263 cash shop items

--- src/app/cash-shop/components/AdvancedItemList.js
+++ src/app/cash-shop/components/AdvancedItemList.js
@@ -165,7 +165,7 @@ function AdvancedItemList() {
         <div className="flex flex-col min-h-dvh h-auto pb-20 bg-cs-bg" style={{ backgroundAttachment: 'fixed' }}>
             <h1 className="text-center text-3xl mb-0 mt-16 text-primary-bright font-bold">Upcoming Cash Shop Sales</h1>
             <h4 className="text-center text-xl my-5 mb-8 italic text-primary">
-                Last Updated for v.262 (August 27th, 2025)
+                Last Updated for v.263 (September 24th, 2025)
             </h4>
 
             <FilterControls
