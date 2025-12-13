193164b21a5e145d2615c0d6434cdc595ad7cffe
Mason Leitch <nosamleitch@gmail.com>
2025-12-12 16:10:49 -0800

    moved cash shop into tools for nav items

--- src/data/navigationItems.js
+++ src/data/navigationItems.js
@@ -7,11 +7,10 @@ export const navigationItems = [
             { name: "Liberation", href: "/liberation", image: '/images/liberation.png', new: false },
         ]
     },
-    {
-        title: "Tools",
-        items: [
-            { name: "Cash Shop", href: "/cash-shop", image: "/images/cash-shop.png" }]
-    },
+    // {
+    //     title: "Tools",
+    //     items: []
+    // },
     {
         title: "Info",
         items: [
@@ -22,6 +21,7 @@ export const navigationItems = [
             // { name: "MAPLE Daily Gift", href: "/maple-daily-gift" },
             // { name: "Inner Ability", href: "/inner-ability" },
             // { name: "Formulas", href: "/formulas", image: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiM4YzhjOGMiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1zcXVhcmUtc2lnbWEiPjxyZWN0IHdpZHRoPSIxOCIgaGVpZ2h0PSIxOCIgeD0iMyIgeT0iMyIgcng9IjIiLz48cGF0aCBkPSJNMTYgOC45VjdIOGw0IDUtNCA1aDh2LTEuOSIvPjwvc3ZnPg==" },
+            { name: "Cash Shop", href: "/cash-shop", image: "/images/cash-shop.png" },
             { name: "Boss Info", href: "/bosses", image: "/images/bosses.png" },
             { name: "Sunny Sunday", href: "/sunny-sunday", image: "/images/sunny-sunday.png", new: true },
         ],
