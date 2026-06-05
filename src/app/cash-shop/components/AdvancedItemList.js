5adcbd30860c92b348ca7890301072564d8cd2fd
Mason Leitch <nosamleitch@gmail.com>
2026-06-05 00:57:00 -0700

    update disclaimer

--- src/app/cash-shop/components/AdvancedItemList.js
+++ src/app/cash-shop/components/AdvancedItemList.js
@@ -194,7 +194,7 @@ function AdvancedItemList() {
             </h4>
 
             <div className="text-center text-sm text-yellow-800 bg-yellow-100 border border-yellow-400 rounded-lg px-4 py-2 mx-auto max-w-2xl mb-4">
-        HUGE DISCLAIMER: THE ITEMS LISTED FOR SALE BEYOND JUNE 10TH USE DATA FROM THE <b>PUBLIC TEST SERVER v.269 BUILD</b>. There is a decent likelihood that these change ahead of time! 
+        HUGE DISCLAIMER: THE ITEMS LISTED FOR SALE BEYOND JUNE 10TH USE DATA FROM THE <b>PUBLIC TEST SERVER v.269 BUILD</b>. There is a possibility, as there always is, that these change ahead of time! 
             </div>
 
             <FilterControls
